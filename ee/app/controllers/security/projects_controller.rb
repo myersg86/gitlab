b2c734d202a8f473eb235a2d77eeff249b5addb4
Avielle Wolfe <awolfe@gitlab.com>
2019-10-16 20:10:57 -0400

    wip

--- ee/app/controllers/security/projects_controller.rb
+++ ee/app/controllers/security/projects_controller.rb
@@ -7,11 +7,27 @@ module Security
     end
 
     def create
-      head :ok
+      result = add_projects(current_user, project_ids)
+
+      render json: {
+        added: result.added_project_ids,
+        duplicate: result.duplicate_project_ids,
+        invalid: result.invalid_project_ids
+      }
     end
 
     def destroy
       head :ok
+    end
+
+    private
+
+    def project_ids
+      params.require(:project_ids)
+    end
+
+    def add_projects(current_user, project_ids)
+      UsersSecurityDashboardProjects::CreateService.new(current_user).execute(project_ids)
     end
   end
 end
