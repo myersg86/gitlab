22b94624499e79b2012e28a18f81d8072fe9b162
Alessio Caiazza <acaiazza@gitlab.com>
2019-11-26 18:51:46 +0100

    Fix db/schema.rb after picking MRs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_023952) do
+ActiveRecord::Schema.define(version: 2019_11_22_135327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
