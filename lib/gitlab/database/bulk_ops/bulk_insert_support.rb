a651e625f32247a49715bb8224f1fa419cb20a90
Matthias Kaeppler <mkaeppler@gitlab.com>
2019-12-12 16:33:49 +0100

    Run after_save hooks after INSERT
    This is a major refactoring that unhinges after_save
    callbacks and replaces them after we run a bulk INSERT

--- lib/gitlab/database/bulk_ops/bulk_insert_support.rb
+++ lib/gitlab/database/bulk_ops/bulk_insert_support.rb
@@ -8,80 +8,134 @@ module Gitlab
       AlreadyActiveError = Class.new(StandardError)
 
       class BulkInsertState
-        attr_reader :captures
-
-        def self.set!(clazz, instance = BulkInsertState.new)
-          p "request bulk inserts for #{clazz}"
-          Thread.current[:_gl_bulk_insert_state] ||= {}
-          Thread.current[:_gl_bulk_insert_state][clazz] = instance
-        end
-
-        def self.unset!(clazz)
-          set!(clazz, nil)
-        end
-
-        def self.get(clazz)
-          Thread.current[:_gl_bulk_insert_state]&.fetch(clazz, nil)
-        end
+        attr_reader :captures, :callbacks
 
         def initialize
           @captures = []
+          @callbacks = nil
         end
 
-        def add_capture!(capture)
+        def add_capture(capture)
           @captures << capture
         end
 
         def inject_last_values!(values)
-          @captures.last[:set_values].call(values)
+          @captures.last[:values] = values
+        end
+
+        def store_callbacks(callbacks)
+          @callbacks = callbacks
         end
       end
 
       class_methods do
         def with_bulk_inserts(&block)
-          raise AlreadyActiveError.new("Cannot nest bulk inserts") if BulkInsertState.get(self)
-
-          # TODO: this should probably be bound to a transaction somehow
-          BulkInsertState.set!(self)
+          raise AlreadyActiveError.new("Cannot nest bulk inserts") if _gl_bulk_inserts_requested?
 
           self.transaction do
+            p "BEGIN TRANSACTION"
+
+            _gl_set_bulk_insert_state
+            _gl_delay_callbacks
+
             yield
-            _gl_bulk_insert!
+
+            _gl_bulk_insert
+            _gl_restore_callbacks
+            _gl_replay_callbacks
+
+            p "END TRANSACTION"
           end
 
         ensure
-          BulkInsertState.unset!(self)
+          _gl_release_bulk_insert_state
         end
 
         def _gl_bulk_inserts_requested?
-          p "requested for #{self} = #{!!BulkInsertState.get(self)}"
-          !!BulkInsertState.get(self)
+          !!_gl_bulk_insert_state
         end
 
-        # pre-conditions: `BulkInsertState` != nil && BulkInsertState.get.captures != nil
-        def _gl_bulk_insert!
-          p "// flushing delayed inserts"
-
-          captures = BulkInsertState.get(self).captures
-
-          values = captures.map { |c| c[:values] }
-
-          ids = Gitlab::Database.bulk_insert(table_name, values, return_ids: true)
-          p "inserted IDs: #{ids.inspect}"
-
-          if ids && ids.any?
-            captures.zip(ids).each { |c, id| c[:set_id].call(id) }
-          end
-          # TODO: assert ids.size == values.size
+        def _gl_capture_record(record)
+          _gl_bulk_insert_state.add_capture(record)
         end
 
         # Overrides ActiveRecord::Persistence::ClassMethods._insert_record
         def _insert_record(values)
           if _gl_bulk_inserts_requested?
             p "!! delaying row #{values} for bulk insert"
-            BulkInsertState.get(self).inject_last_values!(values)
+            _gl_bulk_insert_state.inject_last_values!(values)
           else
             super
+          end
+        end
+
+        private
+
+        def _gl_bulk_insert_state
+          Thread.current[:_gl_bulk_insert_state]&.fetch(self.class, nil)
+        end
+
+        def _gl_set_bulk_insert_state
+          Thread.current[:_gl_bulk_insert_state] = {
+            self.class => BulkInsertState.new
+          }
+        end
+
+        def _gl_release_bulk_insert_state
+          Thread.current[:_gl_bulk_insert_state] = nil
+        end
+
+        def _gl_delay_callbacks
+          # make backup of original callback chain
+          _gl_bulk_insert_state.store_callbacks(_save_callbacks.deep_dup)
+
+          # remove after_save callbacks, so that they won't be invoked
+          _save_callbacks.send(:chain).reject! { |c| c.kind == :after }
+        end
+
+        def _gl_restore_callbacks
+          stored_callbacks = _gl_bulk_insert_state.callbacks
+
+          _save_callbacks = stored_callbacks
+          __callbacks[:save] = stored_callbacks
+        end
+
+        def _gl_replay_callbacks
+          p "REPLAYING after_save"
+          _gl_bulk_insert_state.captures.each do |capture|
+            target = capture[:target]
+            _gl_run_save_callback(target, :after)
+          end
+        end
+
+        # Because `run_callbacks` will run _all_ callbacks for e.g. `save`,
+        # we need to filter out anything that is not of the given `kind` first.
+        def _gl_run_save_callback(target, kind)
+          current_callbacks = _save_callbacks.deep_dup
+
+          requested_chain = _save_callbacks.send(:chain)
+          requested_chain.reject! { |c| c.kind != kind }
+
+          target.run_callbacks :save
+
+          _save_callbacks = current_callbacks
+          __callbacks[:save] = current_callbacks
+        end
+
+        def _gl_bulk_insert
+          p "BULK INSERT"
+
+          # obtains all captured model instances & row values
+          captures = _gl_bulk_insert_state.captures
+
+          values = captures.map { |c| c[:values] }
+
+          ids = Gitlab::Database.bulk_insert(table_name, values, return_ids: true)
+          p "--> row IDs: #{ids.inspect}"
+
+          # inject row IDs back into model instances
+          if ids && ids.any?
+            captures.zip(ids).each { |c, id| c[:target].id = id }
           end
         end
       end
@@ -91,11 +145,13 @@ module Gitlab
       # Overrides ActiveRecord::*._create_record
       def _create_record(*)
         if self.class._gl_bulk_inserts_requested?
-          record_capture = {
-            set_id: ->(id) { self.id ||= id },
-            set_values: -> (values) { record_capture[:values] = values }
-          }
-          BulkInsertState.get(self.class).add_capture!(record_capture)
+          # We need to hold on to the current instance, so that we can
+          # inject IDs into them later on.
+          # The row `values` will be injected later in `_insert_record`/
+          self.class._gl_capture_record({
+            target: self,
+            values: nil
+          })
         end
 
         super
