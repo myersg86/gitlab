7f49bf4f59bc56df7fbc036d36bd729d3e08857d
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-01-30 18:10:58 +0100

    Update store with new paths
    - getters
    - mutations
    - actions_spec
    - getters_spec
    - mutation_spec

--- app/assets/javascripts/registry/settings/store/getters.js
+++ app/assets/javascripts/registry/settings/store/getters.js
@@ -7,4 +7,11 @@ export const getKeepN = state =>
   state.settings.keep_n || findDefaultOption(state.formOptions.keepN);
 export const getOlderThan = state =>
   state.settings.older_than || findDefaultOption(state.formOptions.olderThan);
+export const getSettings = (state, getters) => ({
+  enabled: state.settings.enabled,
+  cadence: getters.getCadence,
+  older_than: getters.getOlderThan,
+  keep_n: getters.getKeepN,
+  name_regex: state.settings.name_regex,
+});
 export const getIsEdited = state => !isEqual(state.original, state.settings);
