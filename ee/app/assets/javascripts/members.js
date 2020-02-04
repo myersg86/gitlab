bc1831471472d2071f6564de038b89d3c30cfb34
Ammar Alakkad <aalakkad@gitlab.com>
2020-02-04 14:28:15 +0300

    Show warning when max seats is increasing
    It will show a confirmation dialog asking the user to confirm
    adding more users to the group when it will result in additional cost.

--- ee/app/assets/javascripts/members.js
+++ ee/app/assets/javascripts/members.js
@@ -5,6 +5,8 @@ import axios from '~/lib/utils/axios_utils';
 import Members from '~/members';
 
 export default class MembersEE extends Members {
+  hasConfirmedAddingMember = false;
+
   addListeners() {
     super.addListeners();
 
@@ -14,6 +16,11 @@ export default class MembersEE extends Members {
     $('.js-ldap-override')
       .off('click')
       .on('click', this.toggleMemberAccessToggle.bind(this));
+    const $addMemberForm = $('form.invite-users-form');
+    $addMemberForm.on('submit', this.showTrueUpModal.bind(this));
+    $('#add-true-up-user-modal')
+      .find('.confirm-adding-member-to-group')
+      .click(this.continueFormSubmitting.bind(this));
   }
 
   dropdownClicked(options) {
@@ -85,6 +92,48 @@ export default class MembersEE extends Members {
           createFlash(__('An error occurred while saving LDAP override status. Please try again.'));
         }
       });
+  }
+
+  showTrueUpModal(e) {
+    const $addMemberForm = $('form.invite-users-form');
+    const membersBeingAdded = $('#user_ids')
+      .val()
+      .split(',').length;
+    const {
+      isFreePlan,
+      maxSeatsUsed,
+      seatsInUse,
+      // seatsOwed,
+      subscriptionSeats,
+    } = $addMemberForm.data();
+
+    const baseUserCount = subscriptionSeats < maxSeatsUsed ? maxSeatsUsed : subscriptionSeats;
+    const overUsers = Math.abs(baseUserCount - (seatsInUse + membersBeingAdded));
+
+    // Show the modal if:
+    // - the group is not on free plan
+    // - this event isn't triggered by submit programmatically
+    // - the max seats is about to increase
+    if (!isFreePlan && !this.hasConfirmedAddingMember && overUsers > 0) {
+      e.preventDefault();
+      $('#add-true-up-user-modal')
+        .modal()
+        .on('hide.bs.modal', () => {
+          if (!this.hasConfirmedAddingMember) {
+            // enable "Add to group" button
+            $addMemberForm
+              .find('input[type="submit"]')
+              .removeClass('disabled')
+              .removeAttr('disabled');
+          }
+        });
+    }
+  }
+
+  continueFormSubmitting() {
+    this.hasConfirmedAddingMember = true;
+    const $addMemberForm = $('form.invite-users-form');
+    $addMemberForm.submit();
   }
 
   static overrideLdap($memberListitem, endpoint, override) {
--- ee/spec/features/groups/members/list_members_spec.rb
+++ ee/spec/features/groups/members/list_members_spec.rb
@@ -2,8 +2,10 @@
 require 'spec_helper'
 
 describe 'Groups > Members > List members' do
+  include Select2Helper
   let(:user1) { create(:user, name: 'John Doe') }
   let(:user2) { create(:user, name: 'Mary Jane') }
+  let(:user_owner) { create(:user, name: 'John Jane') }
   let(:group) { create(:group) }
 
   before do
@@ -28,6 +30,92 @@ describe 'Groups > Members > List members' do
 
       member = GroupMember.find_by(user: user2, group: group)
       expect(find("#group_member_#{member.id}").find('.badge-info')).to have_content('SAML')
+    end
+  end
+
+  describe 'Show warning when adding a member will incur in a charge', :js do
+    let!(:gitlab_subscription) { create(:gitlab_subscription, :gold, namespace: group, seats: 1) }
+
+    before do
+      group.add_owner(user_owner)
+      sign_in(user_owner)
+
+      visit group_group_members_path(group)
+
+      page.within '.invite-users-form' do
+        select2(user2.id, from: '#user_ids', multiple: true)
+        select('Developer', from: 'access_level')
+      end
+    end
+
+    context 'when there are no seats left' do
+      context 'when submitting the "add new member" form' do
+        it 'shows a modal to block the submit going through' do
+          expect(page).to have_selector('.modal#add-true-up-user-modal', visible: false)
+
+          click_button 'Invite'
+
+          expect(page).to have_selector('.modal#add-true-up-user-modal', visible: true)
+        end
+      end
+
+      context 'when confirming the modal' do
+        it 'does not contain the new user in the member list before the submit' do
+          expect(page.find('.members-list')).not_to have_content(user2.name)
+        end
+
+        it 'submits the form and adds the user to the member list' do
+          click_button 'Invite'
+
+          page.within '.modal#add-true-up-user-modal' do
+            click_button 'Add seat'
+          end
+
+          expect(page.find('.members-list')).to have_content(user2.name)
+        end
+      end
+
+      context 'when canceling the modal' do
+        before do
+          click_button 'Invite'
+        end
+
+        context 'when clicking the close button' do
+          it 'closes the modal without submitting the form' do
+            page.find('.modal#add-true-up-user-modal .close').click
+
+            expect(page).to have_selector('.modal#add-true-up-user-modal', visible: false)
+          end
+        end
+
+        context 'when clicking the cancel button' do
+          it 'closes the modal without submitting the form' do
+            page.within '.modal#add-true-up-user-modal' do
+              click_button 'Cancel'
+            end
+
+            expect(page).to have_selector('.modal#add-true-up-user-modal', visible: false)
+          end
+        end
+
+        it 'enables the "add to group" button' do
+          expect(page).to have_button('Invite', disabled: true)
+
+          page.find('.modal#add-true-up-user-modal .close').click
+
+          expect(page).to have_button('Invite', disabled: false)
+        end
+      end
+    end
+
+    context 'when there are seats left' do
+      context 'when submitting the "add new member" form' do
+        it 'does not show a modal to block the submit going through' do
+          click_button 'Invite'
+
+          expect(page).to have_selector('.modal#add-true-up-user-modal', visible: false)
+        end
+      end
     end
   end
 end
