eb726bfdf74bab58cf6a4663f348cb6e274b741a
Małgorzata Ksionek <mksionek@gitlab.com>
2020-02-25 11:46:42 +0100

    Add cr remarks

--- ee/spec/models/group_push_rule_spec.rb
+++ ee/spec/models/group_push_rule_spec.rb
@@ -14,7 +14,7 @@ describe GroupPushRule do
     it { is_expected.to validate_numericality_of(:max_file_size).is_greater_than_or_equal_to(0).only_integer }
 
     it 'validates RE2 regex syntax' do
-      push_rule = build(:push_rule, branch_name_regex: '(ee|ce).*\1')
+      push_rule = build(:group_push_rule, branch_name_regex: '(ee|ce).*\1')
 
       expect(push_rule).not_to be_valid
       expect(push_rule.errors.full_messages.join).to match /invalid escape sequence/
@@ -22,13 +22,13 @@ describe GroupPushRule do
   end
 
   it 'defaults regexp_uses_re2 to true' do
-    push_rule = create(:push_rule)
+    push_rule = create(:group_push_rule)
 
     expect(push_rule.regexp_uses_re2).to eq(true)
   end
 
   it 'updates regexp_uses_re2 to true on edit' do
-    push_rule = create(:push_rule, regexp_uses_re2: nil)
+    push_rule = create(:group_push_rule, regexp_uses_re2: false)
 
     expect do
       push_rule.update!(branch_name_regex: '.*')
