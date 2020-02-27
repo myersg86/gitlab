05bc89be4ab4dda46e80b4fb2c45be76e9b4ffba
Florie Guibert <fguibert@gitlab.com>
2020-02-27 10:08:17 +1000

    Fix scoped label text color for light background
    Improve readability for light color scoped labels

--- ee/app/helpers/ee/labels_helper.rb
+++ ee/app/helpers/ee/labels_helper.rb
@@ -19,6 +19,7 @@ module EE
         bg_color: label.color
       ) + render_label_text(
         label_name,
+        css_class: ('gl-label-text-dark' if light_color?(label.color)),
         suffix: suffix
       )
     end
