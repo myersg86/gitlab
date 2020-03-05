b8a504c0231c8e84db39bf680d856dfb72417e09
Justin Ho <hduong@gitlab.com>
2020-03-05 20:45:13 +0700

    Update copy for Service Templates deprecation

--- spec/views/admin/services/index.html.haml_spec.rb
+++ spec/views/admin/services/index.html.haml_spec.rb
@@ -7,23 +7,23 @@ describe 'admin/services/index.html.haml' do
     assign(:services, build_stubbed_list(:service, 1))
   end
 
-  context 'user has not dismissed Service Templates deperacation message' do
+  context 'user has not dismissed Service Templates deprecation message' do
     it 'shows the message' do
       allow(view).to receive(:show_service_templates_deprecated?).and_return(true)
 
       render
 
-      expect(rendered).to have_content('Service templates will be deprecated')
+      expect(rendered).to have_content('Service Templates will soon be deprecated.')
     end
   end
 
-  context 'user has dismissed Service Templates deperacation message' do
+  context 'user has dismissed Service Templates deprecation message' do
     it 'does not show the message' do
       allow(view).to receive(:show_service_templates_deprecated?).and_return(false)
 
       render
 
-      expect(rendered).not_to have_content('Service templates will be deprecated')
+      expect(rendered).not_to have_content('Service Templates will soon be deprecated.')
     end
   end
 end
