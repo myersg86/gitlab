51d7010077d7480a558373dc9682501abcc6ae94
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2020-07-20 06:28:03 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- ee/app/graphql/types/vulnerable_projects_by_grade_type.rb
+++ ee/app/graphql/types/vulnerable_projects_by_grade_type.rb
@@ -4,7 +4,7 @@ module Types
   # rubocop: disable Graphql/AuthorizeTypes
   class VulnerableProjectsByGradeType < BaseObject
     graphql_name 'VulnerableProjectsByGrade'
-    description 'Represents vulnerability letter grades wit associated projects'
+    description 'Represents vulnerability letter grades with associated projects'
 
     field :grade, Types::VulnerabilityGradeEnum, null: false,
           description: "Grade based on the highest severity vulnerability present"
