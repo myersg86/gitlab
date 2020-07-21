be7095348a217d6172641f56ed0dda293d9b85d3
Tetiana Chupryna <tchupryna@gitlab.com>
2020-07-21 14:35:55 +0300

    Address review comments

--- db/post_migrate/20200714124528_migrate_license_management_artifacts_to_license_scanning.rb
+++ db/post_migrate/20200714124528_migrate_license_management_artifacts_to_license_scanning.rb
@@ -12,14 +12,16 @@ class MigrateLicenseManagementArtifactsToLicenseScanning < ActiveRecord::Migrati
 
     belongs_to :job, class_name: "Ci::Build", foreign_key: :job_id
 
-    scope :duplicates, -> { select("MIN(id) as id").group(:job_id).map(&:id) }
+    scope :original_reports, -> { select("MIN(id) as id").group(:job_id).pluck(&:id) }
     scope :license_compliance, -> { where(file_type: [10, 101]) }
   end
 
   def up
-    JobArtifact.license_compliance.where.not(id: JobArtifact.license_compliance.duplicates).delete_all
-    JobArtifact.where(file_type: 10).update(file_type: 101)
+    JobArtifact.license_compliance.where.not(id: JobArtifact.license_compliance.original_reports).delete_all
+    JobArtifact.where(file_type: 10).update_all(file_type: 101)
   end
 
-  def down; end
+  def down
+    # this one way migration as we're updating data
+  end
 end
