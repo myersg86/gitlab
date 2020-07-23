6c2ba89636ecef30c3d835cfe21e6836fd513e12
Sean Arnold <sarnold@gitlab.com>
2020-07-24 11:21:12 +1200

    Don't use factories in migration spec

--- spec/migrations/migrate_incident_issues_to_incident_type_spec.rb
+++ spec/migrations/migrate_incident_issues_to_incident_type_spec.rb
@@ -5,34 +5,51 @@ require Rails.root.join('db', 'post_migrate', '20200723040950_migrate_incident_i
 
 RSpec.describe MigrateIncidentIssuesToIncidentType do
   let(:migration) { described_class.new }
+
+  let(:projects) { table(:projects) }
+  let(:namespaces) { table(:namespaces) }
+  let(:labels) { table(:labels) }
+  let(:issues) { table(:issues) }
+  let(:label_links) { table(:label_links) }
   let(:label_props) { IncidentManagement::CreateIncidentLabelService::LABEL_PROPERTIES }
-  let(:label) { create(:label, label_props) }
-  let!(:incident_issue) { create(:issue, labels: [label], author: User.alert_bot)}
-  let!(:other_issue) { create(:issue) }
+
+  let(:namespace) { namespaces.create!(name: 'foo', path: 'foo') }
+  let!(:project) { projects.create!(namespace_id: namespace.id) }
+  let(:label) { labels.create!(project_id: project.id, **label_props) }
+  let!(:incident_issue) { issues.create!(project_id: project.id, author_id: User.alert_bot.id) }
+  let!(:other_issue) { issues.create!(project_id: project.id) }
+
+  # Issue issue_type enum
+  let(:issue_issue_type) { 0 }
+  let(:incident_issue_type) { 1 }
+
+  before do
+    label_links.create!(target_id: incident_issue.id, label_id: label.id, target_type: 'Issue')
+  end
 
   describe '#up' do
     it 'updates the incident issue type' do
       expect { migrate! }
         .to change { incident_issue.reload.issue_type }
-        .from('issue')
-        .to('incident')
+        .from(issue_issue_type)
+        .to(incident_issue_type)
 
-      expect(other_issue.reload.issue_type).to eql('issue')
+      expect(other_issue.reload.issue_type).to eql(issue_issue_type)
     end
   end
 
   describe '#down' do
-    let!(:incident_issue) { create(:incident, labels: [label], author: User.alert_bot)}
+    let!(:incident_issue) { issues.create!(project_id: project.id, author_id: User.alert_bot.id, issue_type: incident_issue_type) }
 
     it 'updates the incident issue type' do
       migration.up
 
       expect { migration.down }
         .to change { incident_issue.reload.issue_type }
-        .from('incident')
-        .to('issue')
+        .from(incident_issue_type)
+        .to(issue_issue_type)
 
-      expect(other_issue.reload.issue_type).to eql('issue')
+      expect(other_issue.reload.issue_type).to eql(issue_issue_type)
     end
   end
 end
