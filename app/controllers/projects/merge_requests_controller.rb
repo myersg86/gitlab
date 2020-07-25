fb47f747fafd595f24ac435b199678fe5d8a58f9
Samantha Ming <sming@gitlab.com>
2020-07-24 21:04:32 -0700

    Create the jump to next unresolved feature flag
    Issue: https://gitlab.com/gitlab-org/gitlab/-/issues/199718

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -36,6 +36,7 @@ class Projects::MergeRequestsController < Projects::MergeRequests::ApplicationCo
     push_frontend_feature_flag(:multiline_comments, @project)
     push_frontend_feature_flag(:file_identifier_hash)
     push_frontend_feature_flag(:batch_suggestions, @project, default_enabled: true)
+    push_frontend_feature_flag(:show_jump_to_next_unresolved_in_threads, @project, default_enabled: true)
   end
 
   before_action do
