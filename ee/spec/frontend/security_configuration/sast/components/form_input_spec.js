53f2aecd75b1cff247391e66d9cf1d3cbd955b01
Mark Florian <mflorian@gitlab.com>
2020-07-27 18:40:35 +0100

    Add FormInput component
    This adds a form input component, to be used in the [SAST Configuration
    UI][epic].
    
    This component is a fairly thin wrapper around the `GlFormGroup` and
    `GlFormInput` components, to conform to the [design]. It will be
    consumed by a [parent container component][container], itself driven via
    a schema to generate a form.
    
    Addresses https://gitlab.com/gitlab-org/gitlab/-/issues/225224.
    
    [epic]: https://gitlab.com/groups/gitlab-org/-/epics/3659
    [container]: https://gitlab.com/gitlab-org/gitlab/-/issues/231370
    [design]: https://gitlab.com/gitlab-org/gitlab/-/issues/228830

---
+++ ee/spec/frontend/security_configuration/sast/components/form_input_spec.js
@@ -0,0 +1,125 @@
+import { mount } from '@vue/test-utils';
+import { GlFormInput } from '@gitlab/ui';
+import FormInput from 'ee/security_configuration/sast/components/form_input.vue';
+import { SCHEMA_TO_PROP_SIZE_MAP } from 'ee/security_configuration/sast/components/constants';
+
+describe('FormInput component', () => {
+  let wrapper;
+
+  const testProps = {
+    field: 'field',
+    label: 'label',
+    description: 'description',
+    defaultValue: 'defaultValue',
+    value: 'defaultValue',
+  };
+  const newValue = 'foo';
+  const customValueMessageRegExp = /using custom settings/i;
+
+  const createComponent = ({ props = {} } = {}) => {
+    wrapper = mount(FormInput, {
+      propsData: {
+        ...props,
+      },
+    });
+  };
+
+  const findInput = () => wrapper.find('input');
+  const findLabel = () => wrapper.find('label');
+  const findInputComponent = () => wrapper.find(GlFormInput);
+
+  afterEach(() => {
+    wrapper.destroy();
+    wrapper = null;
+  });
+
+  describe('label', () => {
+    beforeEach(() => {
+      createComponent({
+        props: testProps,
+      });
+    });
+
+    it('renders the label', () => {
+      expect(findLabel().text()).toContain(testProps.label);
+    });
+
+    it('renders the description', () => {
+      expect(findLabel().text()).toContain(testProps.description);
+    });
+  });
+
+  describe('input', () => {
+    beforeEach(() => {
+      createComponent({
+        props: testProps,
+      });
+    });
+
+    it('sets the input to the value', () => {
+      expect(findInput().element.value).toBe(testProps.value);
+    });
+
+    it('is connected to the label', () => {
+      expect(findInput().attributes('id')).toBe(testProps.field);
+      expect(findLabel().attributes('for')).toBe(testProps.field);
+    });
+
+    describe('when the user changes the value', () => {
+      beforeEach(() => {
+        findInput().setValue(newValue);
+      });
+
+      it('emits an input event with the new value', () => {
+        expect(wrapper.emitted('input')).toEqual([[newValue]]);
+      });
+    });
+  });
+
+  describe('custom value message', () => {
+    describe('given the value equals the custom value', () => {
+      beforeEach(() => {
+        createComponent({
+          props: testProps,
+        });
+      });
+
+      it('does not display the custom value message', () => {
+        expect(wrapper.text()).not.toMatch(customValueMessageRegExp);
+      });
+    });
+
+    describe('given the value differs from the custom value', () => {
+      beforeEach(() => {
+        createComponent({
+          props: {
+            ...testProps,
+            value: newValue,
+          },
+        });
+      });
+
+      it('does not display the custom value message', () => {
+        expect(wrapper.text()).toMatch(customValueMessageRegExp);
+      });
+    });
+  });
+
+  // TODO: unskip once
+  // https://gitlab.com/gitlab-org/gitlab-ui/-/merge_requests/1605 merges.
+  describe.skip('size prop', () => {
+    it.each(Object.entries(SCHEMA_TO_PROP_SIZE_MAP))(
+      'maps the %p size prop to %p',
+      (size, mappedSize) => {
+        createComponent({
+          props: {
+            ...testProps,
+            size,
+          },
+        });
+
+        expect(findInputComponent().props('size')).toBe(mappedSize);
+      },
+    );
+  });
+});
