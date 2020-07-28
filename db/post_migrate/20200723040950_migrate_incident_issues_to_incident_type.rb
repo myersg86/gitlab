e01ae8b4ed106d7b88a4faa8adfc0d98e7214adf
Sean Arnold <sarnold@gitlab.com>
2020-07-28 14:49:37 +1200

    Update migration to use custom model

--- db/post_migrate/20200723040950_migrate_incident_issues_to_incident_type.rb
+++ db/post_migrate/20200723040950_migrate_incident_issues_to_incident_type.rb
@@ -5,23 +5,41 @@ class MigrateIncidentIssuesToIncidentType < ActiveRecord::Migration[6.0]
 
   INCIDENT_ISSUE_TYPE = 1
   ISSUE_ISSUE_TYPE = 0
+  BATCH_SIZE = 1000
 
-  # Add each_batch functionality to Issue
-  Issue.send(:include, 'EachBatch'.constantize)
+  LABEL_PROPERTIES = {
+    title: 'incident',
+    color: '#CC0033',
+    description: <<~DESCRIPTION.chomp
+      Denotes a disruption to IT services and \
+      the associated issues require immediate attention
+    DESCRIPTION
+  }.freeze
+
+  class Issue < ActiveRecord::Base
+    include EachBatch
+
+    scope :incident_labelled, -> do
+      joins("INNER JOIN label_links ON label_links.target_type = 'Issue' AND label_links.target_id = issues.id")
+        .joins("INNER JOIN labels ON labels.id = label_links.label_id")
+        .where(labels: { title: LABEL_PROPERTIES[:title], color: LABEL_PROPERTIES[:color], description: LABEL_PROPERTIES[:description] })
+    end
+
+    scope :incident_issues, -> { where(issue_type: INCIDENT_ISSUE_TYPE) }
+  end
 
   def up
-    incident_issues = Issue.authored(User.alert_bot)
-                      .with_label_attributes(IncidentManagement::CreateIncidentLabelService::LABEL_PROPERTIES)
+    incident_issues = Issue.incident_labelled
 
-    incident_issues.each_batch do |batch|
+    incident_issues.each_batch(of: BATCH_SIZE) do |batch|
       batch.update_all(issue_type: INCIDENT_ISSUE_TYPE )
     end
   end
 
   def down
-    incident_issues = Issue.incident
+    incident_issues = Issue.incident_issues
 
-    incident_issues.each_batch do |batch|
+    incident_issues.each_batch(of: BATCH_SIZE) do |batch|
       batch.update_all(issue_type: ISSUE_ISSUE_TYPE )
     end
   end
